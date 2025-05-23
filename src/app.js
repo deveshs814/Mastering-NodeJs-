b5f76dd614956d05a219e2f5d0eef2013d55a08b
Devesh Shukla <shukladevesh9814@gmail.com>
2025-05-23 23:51:54 +0530

    Database connection .

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
-const express  = require("express");
-
+const express = require("express");
+const connectDB = require('./config/database.js');
 const app = express();
 
-const { adminAuth } = require("./middleware/auth.js")
-
-app.use("/admin",adminAuth); 
-
-app.get('/admin/getAllData', (req,res) =>{
-    res.send("All data is received")
+connectDB()
+.then(() =>{
+    console.log("Database connection established!!")
 })
-app.get('/admin/deleteAllData',  adminAuth, (req,res) =>{ //this is more better way to pas the middleware in between the route and
-    //(req,res) cycle.
-    res.send("All data is received")
-})
-
-
-app.listen(3000,(req,res) =>{
-    console.log("Server is running at the port 3000");
-})
+.catch((err) =>{
+    console.log("Database cannot be connected!!")
+});

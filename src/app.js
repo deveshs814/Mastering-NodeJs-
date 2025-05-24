13b451e64b48d423c0893c7ff0da32537677e6b1
Devesh Shukla <shukladevesh9814@gmail.com>
2025-05-24 16:30:06 +0530

    created a database and pushed data inside it.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const connectDB = require('./config/database.js');
+const User = require("./models/user.js");
 const app = express();
+
+app.post("/signup",async(req,res) =>{
+    //Creating a new instance of user model
+    const user = new User({
+        firstName: "Devesh",
+        lastName:"Shukla",
+        emailId:"shukla1@gmail.com",
+        password:"12345678"
+    });
+    //this will save the user
+    try {
+        await user.save();
+        res.send("User Added Successfully!")
+    } catch (error) {
+        res.status(400).send("Error saving the user: " + error.message);
+    }
+});
 
 connectDB()
 .then(() =>{
     console.log("Database connection established!!")
+    app.listen(7777,() =>{
+        console.log("Server is successfully listening to the port 7777..!!");
+    })
 })
 .catch((err) =>{
     console.log("Database cannot be connected!!")
--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async() =>{
     await mongoose.connect(
-      //  "mongodb connection string"
+      "mongodb+srv://shukladevesh9814:mahadev123@devtinder1.hvulopz.mongodb.net/devtinder1"
     );
 };
 
 module.exports = connectDB;
+
+//devtinder1.hvulopz.mongodb.net

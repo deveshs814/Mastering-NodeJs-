92e099ed5c7931bb22f9ed5b6e8459c2a21e8a68
Devesh Shukla <shukladevesh9814@gmail.com>
2025-05-27 02:26:47 +0530

    Data model santization and schema validations.

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,47 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName:{
-        type: String
+        type: String,
+        minLength:6,
+        maxLength:20,
+        required: true
     },
     lastName:{
-        type: String
+        type: String,
     },
     emailId:{
-        type: String
+        type: String,
+        lowercase:true,
+        unique:true,
+        trim:true,
+        required: true
     },
     password:{
-        type:String
+        type:String,
+        required: true
     },
     age:{
-        type: Number
+        type: Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){ //validation is checked before the saving into db..will save only if validation is okay.
+            if(!["male","female","others"].includes(value)){
+                throw new error("Gender is not valid");
+            }
+        },
+    },
+    about:{
+        type:String,
+        default:"This is a default about of user" //this will be the default value of about if not passed by user.
+    },
+    skills:{
+        type:[String],
     }
+},{
+    timestamps:true, //this will give the created at and updated at.
 });
 
 const User = mongoose.model("User",userSchema);

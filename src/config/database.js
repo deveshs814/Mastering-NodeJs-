e44596737a87ec5a37861bf5921ba2357bee1c77
Devesh Shukla <shukladevesh9814@gmail.com>
2025-05-26 20:59:27 +0530

    comment

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async() =>{
     await mongoose.connect(
-      "mongodb+srv://shukladevesh9814:mahadev123@devtinder1.hvulopz.mongodb.net/devtinder1"
+    //    "mongodb connection string"
     );
 };
 

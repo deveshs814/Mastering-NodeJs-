49714281ef998ab078eedad752d3f25db2b9ecb0
Devesh Shukla <shukladevesh9814@gmail.com>
2025-06-17 22:15:54 +0530

    Implemented cors.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ const authRouter = require("../src/routes/auth.js");
 const profileRouter  = require("../src/routes/profile.js");
 const requestRouter = require("../src/routes/requests.js");
 const userRouter = require("../src/routes/user.js")
+const cors = require("cors");
 
+app.use(cors(
+    {origin:"http://localhost:5173",
+        credentials:true,
+    }
+))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/user", userRouter);
